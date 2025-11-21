ad1c5013c12dda4db0e9ea6fd88e5f30366e27b3
somebudyelse-ka <somebudyelse@proton.me>
2025-11-21 07:42:30 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-sudo apt install -y qemu-utils qemu-system-x86 novnc python3-websockify python3-numpy
+sudo apt update; sudo apt install -y qemu-utils qemu-system-x86 novnc python3-websockify python3-numpy
 openssl req -x509 -nodes -newkey rsa:3072 -keyout novnc.pem -out novnc.pem -days 3650
 websockify -D --web=/usr/share/novnc/ --cert=./novnc.pem 6080 localhost:5901
